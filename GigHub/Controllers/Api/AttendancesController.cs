4a619dbbed46c3f2c872bfbc2877ecc30f619e20
Arnab <arnab_pmk@yahoo.com>
2017-12-13 18:54:33 +0530

    added gigs functionality

---
+++ GigHub/Controllers/Api/AttendancesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Web.Http;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers.Api
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+           
+            Debug.WriteLine(""+(dto==null));
+
+
+   
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+            {
+                return BadRequest("The attendance already exists");
+            }
+            var attendance = new Attendance()
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+             
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+        
+    }
+}
